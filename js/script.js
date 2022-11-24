4d257649516ccb0fb6edf31928fe3a1d5d9388e8
RiccardoNestola <riccardinux@gmail.com>
2022-11-24 20:30:33 +0100

    add mail

--- js/script.js
+++ js/script.js
@@ -26,3 +26,44 @@ for (let index = 0; index < 50; index++) {
     }
 }
 
+
+/* 
+Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato sull’esito del controllo.
+Prima di partire a scrivere codice poniamoci qualche domanda: Che ci sia un array da qualche parte? Se dobbiamo confrontare qualcosa che "cosa" ci serve?
+*/
+
+
+const listaMail = [
+    "mario@gmail.com",
+    "simone@gmail.com",
+    "stefano@gmail.com",
+    "luca@gmail.com",
+    "fabrizio@gmail.com",
+    "giuseppe@gmail.com",
+];
+
+const mailElement = document.getElementById("mail");
+const button = document.getElementById('pulsante');
+
+button.addEventListener('click', function(){
+    const mailUser = (mailElement.value);
+    const result = document.querySelector('p');
+    let mailFinder = false;
+
+    for (let i = 0; i < listaMail.length; i++) {
+        
+        if (mailUser == (listaMail[i])){
+            mailFinder= true;
+        } 
+    }
+
+    if (mailFinder){
+        result.innerHTML = "Ci sei!"
+    } else {
+        result.innerHTML = "non ci sei ;)"
+    }
+
+})
+
+
